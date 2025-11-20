053b6ad72b2b94227e44bf2ba5736c538534998f
FrankElyy <frank_ely_vivas@dlsl.edu.ph>
2025-11-20 13:12:30 +0800

    Add updater

--- ParkPal.java
+++ ParkPal.java
@@ -4,3 +4,17 @@ import java.util.Scanner;
 abstract class ParkingStatusUpdater {
     public abstract void updateStatus(ParkingSpot spot);
 }
+
+class SensorUpdater extends ParkingStatusUpdater {
+    public void updateStatus(ParkingSpot spot) {
+        spot.setOccupied(true);
+        System.out.println("Sensor detected a vehicle in spot " + spot.getSpotId());
+    }
+}
+
+class ManualUpdater extends ParkingStatusUpdater {
+    public void updateStatus(ParkingSpot spot) {
+        spot.setOccupied(false);
+        System.out.println("Spot " + spot.getSpotId() + " manually released.");
+    }
+}
