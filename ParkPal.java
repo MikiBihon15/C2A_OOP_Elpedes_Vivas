d95d2eded74972c2adea238eb56b27e42ca2eead
FrankElyy <frank_ely_vivas@dlsl.edu.ph>
2025-11-21 00:42:15 +0800

    Update ParkPal.java

--- ParkPal.java
+++ ParkPal.java
@@ -32,3 +32,20 @@ class ParkingSpot {
     public boolean isOccupied() { return isOccupied; }
     public void setOccupied(boolean occupied) { this.isOccupied = occupied; }
 }
+
+// user
+class User {
+    private String email;
+    private String role;
+    private String idNumber; // NEW 🔹
+
+    public User(String email, String role, String idNumber) {
+        this.email = email;
+        this.role = role;
+        this.idNumber = idNumber;
+    }
+
+    public void login() {
+        System.out.println(role + " account logged in: " + email + " | ID: " + idNumber);
+    }
+}
